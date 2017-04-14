9c3ba1a5c856a42bd1f4382da707f88d5f172091
mariamragab <mariam.ragab@dal.ca>
2017-04-14 00:36:40 -0300

    Create game.java

---
+++ game.java
@@ -0,0 +1,11 @@
+public class Game{
+  private ArrayList <Cards> hand;
+  private ArrayList <Cards> compHand;
+  
+  public Game(){
+     hand= new ArrayList <Cards> ();
+     compHand= new ArrayList <Cards> ();
+  }
+  public void deal(int n){
+  
+  }
